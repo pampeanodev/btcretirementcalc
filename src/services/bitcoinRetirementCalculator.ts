f67a80a667dddcce3cae742666bec0e665aec9dd
Pampeano Dev <pampeanodev@gmail.com>
2024-04-22 09:24:43 +0200

    remove commented code

--- src/services/bitcoinRetirementCalculator.ts
+++ src/services/bitcoinRetirementCalculator.ts
@@ -82,43 +82,7 @@ const buildRetirementPrediction = (
       annualRetirementBudget: dataSetItem.desiredAnnualBudgetIndexed,
     });
   }
-  // for (let age = input.currentAge + 1; age <= input.lifeExpectancy; age++) {
-  //   year++;
-  //   bitcoinPrice = bitcoinPrice * growthFactor;
-  //   indexedDesiredAnnualBudget = indexedDesiredAnnualBudget * inflationFactor;
-  //   const pendingSavingsFiat = calculateFiatWillNeedOverLife(age, bitcoinPriceHistory);
 
-  //   // check whether the user has reached as much as they can retire
-  //   if (pendingSavingsFiat <= accumulatedSavingsFiat) {
-  //     calculationResult.canRetire = true;
-  //     const yearsAfterRetirement = input.lifeExpectancy - age;
-  //     calculationResult.annualRetirementBudget = accumulatedSavingsFiat / yearsAfterRetirement;
-  //     calculationResult.annualRetirementBudgetAtRetirementAge = indexedDesiredAnnualBudget;
-  //     calculationResult.retirementAge = age;
-  //     calculationResult.bitcoinPriceAtRetirementAge = bitcoinPrice;
-  //     calculationResult.savingsBitcoin = accumulatedSavingsBitcoin;
-  //     calculationResult.savingsFiat = accumulatedSavingsFiat;
-  //     break;
-  //   }
-  //   // increase bitcoin price as composite interest based on annual price growth
-  //   indexedAnnualBuyInFiat = indexedAnnualBuyInFiat * inflationFactor;
-  //   // accumulate amount of btc you hodl
-  //   const bitcoinToBuy = indexedAnnualBuyInFiat / bitcoinPrice;
-  //   accumulatedSavingsBitcoin += bitcoinToBuy;
-  //   accumulatedSavingsFiat = accumulatedSavingsBitcoin * bitcoinPrice;
-
-  //   // add current year to dataset
-  //   calculationResult.dataSet.push({
-  //     key: year,
-  //     year: year,
-  //     age: age,
-  //     savingsBtc: accumulatedSavingsBitcoin,
-  //     savingsFiat: accumulatedSavingsFiat,
-  //     bitcoinBought: bitcoinToBuy,
-  //     bitcoinPrice: bitcoinPrice,
-  //     annualRetirementBudget: indexedDesiredAnnualBudget,
-  //   });
-  // }
   // didn't find a retirement age skip pos retirement calculations
   if (!calculationResult.canRetire) {
     return calculationResult;
