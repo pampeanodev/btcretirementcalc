1e173dc06582aae580597930aac16dd3ff5e918d
Pampeano Dev <pampeanodev@gmail.com>
2024-05-01 14:17:27 +0200

    add onchain payments

--- src/components/Misc/Donate.tsx
+++ src/components/Misc/Donate.tsx
@@ -1,12 +1,32 @@
-import { Button, Popover } from "antd";
+import { Button, Popover, Tabs, TabsProps } from "antd";
 import { useTranslation } from "react-i18next";
 import LnInvoice from "./LnInvoice";
+import DonateOnChain from "./OnChain";
+import "./Donate.scss";
 
 const Donate = () => {
   const [t] = useTranslation();
 
+  const tabs: TabsProps["items"] = [
+    {
+      key: "1",
+      label: t("donate.lightning"),
+      children: <LnInvoice />,
+    },
+    {
+      key: "2",
+      label: t("donate.onchain"),
+      children: <DonateOnChain />,
+    },
+  ];
+
   return (
-    <Popover zIndex={2000} content={<LnInvoice />} title={t("donate.qrcode.title")} trigger="click">
+    <Popover
+      zIndex={2000}
+      content={<Tabs className="donate-tabs" defaultActiveKey="1" items={tabs} />}
+      title={t("donate.qrcode.title")}
+      trigger="click"
+    >
       <Button size="small" type="primary">
         {t("donate.donate")}
       </Button>
---
+++ src/components/Misc/OnChain.tsx
@@ -0,0 +1,7 @@
+import { QRCode } from "antd";
+
+const OnChain = () => {
+  return <QRCode value="bc1q8y92hwx02nxs5p6qkdm2322vvh55h3wkqpnrye"></QRCode>;
+};
+
+export default OnChain;
